4a774fb657c5aba42148de00177830155a8bda3f
BilalKalyar-200 <mohsinkalyarpk@gmail.com>
2024-09-28 13:05:51 +0500

    my old work on linked list

---
+++ Assignment 1/Assignment 1/doubly linked list.cpp
@@ -0,0 +1,312 @@
+#include<iostream>
+using namespace std;
+class node {
+	int data;
+	int key;
+	node* pre, * next;
+public:
+	node()
+	{
+		data = 0; key = 0; pre = NULL; next = NULL;
+	}
+	void setdata(int d)
+	{
+		data = d;
+	}
+	int getdata()
+	{
+		return data;
+	}
+	void setkey(int key)
+	{
+		this->key = key;
+	}
+	int getkey()
+	{
+		return key;
+	}
+	void setpre(node* pr)
+	{
+		pre = pr;
+	}
+	node* getpre()
+	{
+		return pre;
+	}
+	void setnext(node* next)
+	{
+		this->next = next;
+	}
+	node* getnext()
+	{
+		return next;
+	}
+};
+class doubly {
+	node* head;
+public:
+	doubly()
+	{
+		head = NULL;
+	}
+	void insert_at_begin(int data, int key)		//Time complexity = O(1)
+	{
+		node* ptr = new node;
+		ptr->setkey(key);
+		ptr->setdata(data);
+		if (head == NULL)
+		{
+			head = ptr;
+			head->setpre(NULL);
+			head->setnext(NULL);
+		}
+		else {
+			ptr->setpre(NULL);
+			ptr->setnext(head);
+			head->setpre(ptr);
+			head = ptr;
+		}
+	}
+	void insert_in_middle(int data, int key, int key2)			//Time complexity = O(n) where n equals to number of nodes in list
+	{
+		node* ptr = new node;
+		ptr->setkey(key);
+		ptr->setdata(data);
+		if (head == NULL)
+		{
+			head = ptr;
+			head->setpre(NULL);
+			head->setnext(NULL);
+		}
+		else {
+			node* temp = head;
+			while (temp != NULL)
+			{
+
+				if (temp->getkey() == key2)
+				{
+					if (temp->getnext() != NULL)	//if not the last node
+					{
+						ptr->setnext(temp->getnext());
+						temp->getnext()->setpre(ptr);
+					}
+					else
+					{
+						ptr->setnext(NULL);
+					}
+
+					ptr->setpre(temp);
+					temp->setnext(ptr);
+					break;
+				}
+				temp = temp->getnext();
+			}
+		}
+		cout << "Key not found\n";
+	}
+	void insert_at_end(int data, int key)	//Time complexity = O(n) where n equals to number of nodes in list
+	{
+		node* ptr = new node;
+		ptr->setkey(key);
+		ptr->setdata(data);
+		if (head == NULL)
+		{
+			head = ptr;
+			head->setpre(NULL);
+			head->setnext(NULL);
+		}
+		else {
+			node* temp = head;
+			while (temp->getnext() != NULL)
+			{
+				temp = temp->getnext();
+			}
+			temp->setnext(ptr);
+			ptr->setpre(temp);
+			ptr->setnext(NULL);
+		}
+	}
+	void delte_first_node()	//Time complexity = O(1)
+	{
+		if (head == NULL)
+		{
+			cout << "Nothing to delete bruhhh\n";
+			return;
+		}
+		if (head->getnext() == NULL)	//in case of only one node
+		{
+			head = NULL;
+			return;
+		}
+		head->getnext()->setpre(NULL);
+		head = head->getnext();
+
+	}
+	void delete_a_node(int key)		//Time complexity = O(n) where n equals to number of nodes in list
+	{
+		if (head == NULL)
+		{
+			cout << "Nothing to delete bruhhh\n";
+			return;
+		}
+		node* temp = head;
+		if (head->getnext() == NULL)	//in case of only one node
+		{
+			head = NULL;
+			return;
+		}
+		if (head->getkey() == key)
+		{
+			head = head->getnext();
+			head->setpre(NULL);
+			delete temp;
+			temp = NULL;
+		}
+		while (temp != NULL)
+		{
+			if (temp->getkey() == key)
+			{
+				if (temp->getnext() != NULL)	//if not the last node
+				{
+					temp->getnext()->setpre(temp->getpre());
+				}
+				temp->getpre()->setnext(temp->getnext());
+				delete temp;
+				temp = NULL;
+				return;
+			}
+			temp = temp->getnext();
+		}
+	}
+	void delete_last()
+	{
+		if (head == NULL)
+		{
+			cout << "Nothing to delete bruhhh\n";
+			return;
+		}
+		node* temp = head;
+		if (head->getnext() == NULL)	//in case of only one node
+		{
+			head = NULL;
+			return;
+		}
+		while (temp->getnext() != NULL)
+		{
+			temp = temp->getnext();
+		}
+		temp->getpre()->setnext(NULL);
+		delete temp;
+		temp = NULL;
+	}
+	void search(int data) //Time complexity = O(n) where n equals to number of nodes in list
+	{
+		if (head == NULL)
+		{
+			cout << "Nothing to showw bruhhh\n";
+			return;
+		}
+		else {
+			node* temp = head;
+			while (temp->getnext() != NULL)
+			{
+				if (temp->getdata() == data)
+				{
+					cout << "Key: " << temp->getkey() << " Data: " << temp->getdata() << endl;
+					return;
+				}
+				temp = temp->getnext();
+			}
+		}
+		cout << "No node found with this data: \n";
+	}
+	void display()          //Time complexity = O(n) where n equals to number of nodes in list
+	{
+		node* temp = head;
+		while (temp != NULL)
+		{
+			cout << "Key: " << temp->getkey() << " Data: " << temp->getdata() << endl;
+			temp = temp->getnext();
+		}
+	}
+};
+void input(int& data, int& key)
+{
+	cout << "Enter data: ";
+	cin >> data;
+	cout << "Enter key: ";
+	cin >> key;
+}
+int main()
+{
+	doubly obj;
+	int data, key, ch;
+	while (true)
+	{
+		cout << "Menu:\n";
+		cout << "1.Insert node at beginning\n";
+		cout << "2.Insert node in middle\n";
+		cout << "3.Insert node at end\n";
+		cout << "4.Delete first node\n";
+		cout << "5.Delete node\n";
+		cout << "6.Delete last node\n";
+		cout << "7.Display list\n";
+		cout << "8.Search node\n";
+		cout << "9.Exit\n";
+		cout << "Enter your choice: ";
+		cin >> ch;
+
+		if (ch == 1)
+		{
+			input(data, key);
+			obj.insert_at_begin(data, key);
+		}
+		else if (ch == 2)
+		{
+			input(data, key);
+			int key2;
+			cout << "Enter key of the node after which you want to insert: ";
+			cin >> key2;
+			obj.insert_in_middle(data, key, key2);
+		}
+		else if (ch == 3)
+		{
+			input(data, key);
+			obj.insert_at_end(data, key);
+		}
+		else if (ch == 4)
+		{
+			obj.delte_first_node();
+		}
+		else if (ch == 5)
+		{
+			cout << "Enter key to delete: ";
+			int key2; cin >> key2;
+			obj.delete_a_node(key2);
+		}
+		else if (ch == 6)
+		{
+			obj.delete_last();
+		}
+		else if (ch == 7)
+		{
+			obj.display();
+		}
+		else if (ch == 8)
+		{
+			cout << "Enter data to search: ";
+			int d; cin >> d;
+			obj.search(d);
+		}
+		else if (ch == 9)
+		{
+			cout << "exititing\n";
+			break;
+		}
+		else {
+			cout << "Enter valid choice\n";
+			continue;
+		}
+	}
+	return 0;
+}
---
+++ Assignment 1/Assignment 1/pigLatin form of string.cpp
@@ -0,0 +1,145 @@
+#include<iostream>
+using namespace std;
+class node {
+public:
+	char ch;
+	node* next, * pre;
+	node()
+	{
+		ch = '\0';
+		next = NULL;
+		pre = NULL;
+	}
+
+};
+bool is_vowel(char ch)
+{
+	switch (ch)//as asked treating y as vowel also
+	{
+	case 'a': case 'e': case 'i': case 'o': case 'u':
+	case 'A': case 'E': case 'I': case 'O': case 'U': case'y': case'Y':
+		return true;
+	default:
+		return false;
+	}
+}
+class linked {
+	node* head;
+
+public:
+	linked()
+	{
+		head = NULL;
+	}
+
+	void append(char ch)
+	{
+		node* ptr = new node;
+		ptr->ch = ch;
+		if (head == NULL)
+		{
+			head = ptr;
+			head->next = head;
+			head->pre = head;
+			return;
+		}
+		else {
+			node* temp = head;
+			while (temp->next != head)
+			{
+				temp = temp->next;
+			}
+			temp->next = ptr;
+			ptr->next = head;
+		}
+	}
+	void  reverse()
+	{
+		if (head == NULL)
+		{
+			cout << "Nothing to change\n";
+			return;
+		}
+		bool vowel3 = false;
+		node* temp2 = head;
+		while (temp2->next != head)
+		{
+			if (is_vowel(temp2->ch))		//checking if there is any vowel in string
+			{
+				vowel3 = true;
+				break;
+			}
+			temp2 = temp2->next;
+		}
+		bool isvowel = false, isvowel2 = false;
+		if (vowel3)
+		{
+			node* temp = head;
+			while (temp->next != head)
+			{
+				if ((is_vowel(head->ch)))		//if we found vowel at head
+				{
+					isvowel = true;
+					break;
+				}
+				else {
+					append('-');
+
+					head = head->next;
+					if ((is_vowel(head->ch)))	//checking if we finds vowel at head after moving
+					{
+						isvowel2 = true;
+
+						break;
+					}
+				}
+
+			}
+		}
+		if (!vowel3) {
+			append('-');
+			append('w');
+			append('a');
+			append('y');
+			return;
+		}
+		if (isvowel)
+		{
+			append('-');
+			append('w');
+			append('a');
+			append('y');
+			return;
+		}
+		if (isvowel2)
+		{
+			append('a');
+			append('y');
+			return;
+		}
+
+	}
+	void display()
+	{
+		node* temp = head;
+		do {
+			cout << temp->ch;
+			temp = temp->next;
+		} while (temp != head);
+		cout << endl;
+	}
+};
+int main()
+{
+	linked obj;
+	cout << "Enter string: ";
+	string str; cin >> str;
+
+	int s = str.length();
+	for (int i = 0; i < s; i++)
+	{
+		obj.append(str[i]);
+	}
+	obj.reverse();
+	obj.display();
+}
